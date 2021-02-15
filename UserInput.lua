1ebbf178b6048205d09b24572279c8d15dc94a68
vljmoreira <vljmoreira@users.noreply.github.com>
2021-02-15 18:39:49 +0000

    Basics of lua

---
+++ UserInput.lua
@@ -0,0 +1,15 @@
+print "Type your name: "
+name = io.read()
+
+if name == "Vera" then
+	print "Nice name"
+elseif name == "joe" then
+	print "hello"
+	else
+		print "damm world scope"
+end
+
+-- loops
+--while true do
+--	print "this is a while do "
+--  end
---
+++ vars.lua
@@ -0,0 +1,42 @@
+-- to execute the lua files command = B 
+-- my vars at lua
+-- Vars supported + / - ^ (power) *
+MyVar =10
+
+OtherVar = 12
+
+print(MyVar,OtherVar)
+
+--long test can
+
+myLongTest = 
+[[ "this is my long test, long test var that must have a set of bracketes"]]
+
+print(myLongTest)
+
+--Concatenation operator
+
+part_1= "Lua é"
+part_2=" muito fixe"
+
+--print all sentace
+full_text=part_1..part_2
+
+print(full_text)
+
+-- more operators
+var_1=100
+var_2=20
+
+myBoolean=(var_1 == var_2)
+
+print(myBoolean)
+
+print(var_1 < var_2)
+print(var_1 > var_2)
+print(var_1 <= var_2)
+print(var_1 >= var_2)
+print(var_1 == var_2)
+--print(var_1 ˜= var_2) not equal to
+
+
