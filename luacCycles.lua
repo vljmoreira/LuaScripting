25a463253e44fb41c8859a348a8041585455703f
Vera Moreira <vljmoreira@gmail.com>
2021-07-07 12:00:15 +0100

    file about cycles

---
+++ luacCycles.lua
@@ -0,0 +1,19 @@
+-- Lua cycles 
+
+
+local listaCompras = {
+    legumes = {
+        cenouras = 1,
+        tomate = 1,
+        pepino = 1
+    }
+    fruta = {
+        mirtilo = 1,
+        cerejas = 2
+    }
+
+}
+--pairs() retorna value - key a orden n e espelicita
+print (listaCompras)
+
+--ipairs() retorna index-value usado para tabelas numericas, tabelas n numericas sao ignoradas
